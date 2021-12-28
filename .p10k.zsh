c4998a81cf49b984a9d76dffb193c031bc000791
aptitudepi <45304559+aptitudepi@users.noreply.github.com>
2021-12-28 17:27:34 -0600

    customize prompt symbol for ubuntu

--- .p10k.zsh
+++ .p10k.zsh
@@ -176,7 +176,7 @@
 
   #################################[ os_icon: os identifier ]##################################
   # OS identifier color.
-  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=255
+  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=001
   # Custom icon.
   # typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='⭐'
 
