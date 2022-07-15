8b84fd7bcda367e417b56f6fd4a0ebff5a1e81bf
aptitudepi <45304559+aptitudepi@users.noreply.github.com>
2022-07-15 13:49:55 -0500

    Edit P10K config for IP color

--- .p10k.zsh
+++ .p10k.zsh
@@ -1428,7 +1428,7 @@
 
   ###############################[ public_ip: public IP address ]###############################
   # Public IP color.
-  typeset -g POWERLEVEL9K_PUBLIC_IP_FOREGROUND=94
+  typeset -g POWERLEVEL9K_PUBLIC_IP_FOREGROUND=021
   # Custom icon.
   # typeset -g POWERLEVEL9K_PUBLIC_IP_VISUAL_IDENTIFIER_EXPANSION='⭐'
 
