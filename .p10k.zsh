f3cfe2c1cea30d9ee5c37a9d69ed641c608adecb
aptitudepi <45304559+aptitudepi@users.noreply.github.com>
2022-07-15 13:52:03 -0500

    Edit P10K config for time color

--- .p10k.zsh
+++ .p10k.zsh
@@ -1515,7 +1515,7 @@
 
   ####################################[ time: current time ]####################################
   # Current time color.
-  typeset -g POWERLEVEL9K_TIME_FOREGROUND=66
+  typeset -g POWERLEVEL9K_TIME_FOREGROUND=021
   # Format for the current time: 09:51:02. See `man 3 strftime`.
   typeset -g POWERLEVEL9K_TIME_FORMAT='%D{%I:%M:%S %p}'
   # If set to true, time will update when you hit enter. This way prompts for the past
